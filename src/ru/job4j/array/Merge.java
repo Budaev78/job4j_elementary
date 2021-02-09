bd5e951da3d0ebccb455060562bdfeb39e1f2414
Asus <v_budaev78@mail.ru>
2021-02-09 19:54:18 +0400

    1. Экзамен. Базовый синтаксис. [#458646]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -4,17 +4,25 @@ public class Merge {
 
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
-        }
-        for (int j = left.length; j < left.length + right.length; j++) {
-            rsl[j] = right[j - left.length];
-        }
-        for (int i = 0; i < rsl.length - 1; i++) {
-            if (rsl[i] > rsl[i + 1]) {
-                int temp = rsl[i];
-                rsl[i] = rsl[i + 1];
-                rsl[i + 1] = temp;
+        int i = 0, j = 0;
+        for (int k = 0; k < rsl.length; k++) {
+
+            if (i > left.length - 1) {
+                int a = right[j];
+                rsl[k] = a;
+                j++;
+            } else if (j > right.length - 1) {
+                int a = left[i];
+                rsl[k] = a;
+                i++;
+            } else if (left[i] < right[j]) {
+                int a = left[i];
+                rsl[k] = a;
+                i++;
+            } else {
+                int b = right[j];
+                rsl[k] = b;
+                j++;
             }
         }
         return rsl;
